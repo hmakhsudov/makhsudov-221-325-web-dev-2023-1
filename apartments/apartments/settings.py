d60a160678a965798d4cd89f55c0c8b970622f12
hmakhsudov <mahsudovhabibullo01@gmail.com>
2023-07-01 21:45:02 +0300

    db

--- apartments/apartments/settings.py
+++ apartments/apartments/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'apartments.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'apartments',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
